b36339bd1a92b2ad3f06d60794edbe3d62d6aa3e
cristinagiacobbe <crigiak@gmail.com>
2023-12-29 22:15:23 +0100

    Solve problem of different key in "News" object (array and string) and fix select form

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const News = [
         id: "1",
         title: "Scoperta di una nuova specie di papera di gomma",
         content: "Un breve articolo sulla recente scoperta di una specie di papera di gomma mai vista prima",
-        tags: "geo",
+        tags: ["geo", "tech"],
         author: "Diana Rossi",
         published: "2023-02-11",
         image: "./images/rubber-duck.jpg",
@@ -14,7 +14,7 @@ const News = [
         id: "2",
         title: "Esplorando le profondità marine: il mistero degli abissi",
         content: "Un viaggio nelle profondità dell'oceano alla scoperta di creature misteriose e inesplorate.",
-        tags: "viaggi",
+        tags: ["viaggi", "geo"],
         author: "Fabio Mari",
         published: "2023-03-14",
         image: "./images/deep-sea.jpg",
@@ -34,33 +34,64 @@ const News = [
         id: 4,
         title: "Arte moderna: oltre i confini convenzionali",
         content: "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste ad artisti emergenti.",
-        tags: "arte",
+        tags: ["arte", "tech"],
         author: "Gabriele Neri",
         published: "2023-05-29",
         image: "./images/modern-art.jpg",
         alt: "modern-art",
     }]
 
-const TagsType = ["politic"]
-News.forEach((eachNew) => {
-    if (!TagsType.includes(eachNew.tags)) {
-        TagsType.push(eachNew.tags)
-    }
-})
-console.log(TagsType);
 
+//Extract each value of key "tags" and push them into a new array
+TagsArray = ["politic"]
+News.forEach((New) => {
+
+    if (typeof New.tags === "object") {
+//case of: ["arte", "tech"]
+        for (let i = 0; i < New.tags.length; i++) {
+            TagsArray.push(New.tags[i])} }
+            else {
+//case of: "cucina"
+                TagsArray.push(New.tags); }
+    }
+
+)
+console.log(TagsArray);
+//create a set of tags (withou duplicate)
+const Set_tags = new Set(TagsArray)
+console.log(Set_tags);
+
+/* create select form with a dinamic logic*/
 const selTags = document.getElementById("tags")
-TagsType.forEach((tagType) => {
-    let newOption = new Option(tagType, tagType);
+Set_tags.forEach((Set_tag) => {
+    let newOption = new Option(Set_tag, Set_tag);
     tags.append(newOption);
 })
-//console.log(tags);
 
-function FilterTag(selectedTag) {
+
+
     const NewsCard = News.filter(SingleFilteredTag => {
-    return SingleFilteredTag.tags === selectedTag   
+    return SingleFilteredTag.tags === "geo"      
 })
-console.log(NewsCard)
-}
+console.log(NewsCard);
 
-FilterTag(TagsType[4])
+NewsCard.forEach((SingleFilteredTag) => {
+    console.log(SingleFilteredTag.title);
+
+const cardElement = document.querySelector(".card")
+const cardMarkup = `
+<div class="card-body">
+            <h4 class="card-title">${SingleFilteredTag.title}</h4>
+            <h5 class="card-title">pubblicato da ${SingleFilteredTag.author}</h5>
+            <h6>in data ${SingleFilteredTag.published}</h6>
+            <p class="card-text">${SingleFilteredTag.content}</p>
+            <div>
+                <img src=${SingleFilteredTag.image} class="mb-2" alt=${SingleFilteredTag.alt}>
+            </div>
+            <a href="#" class="btn btn-primary btn-geo">${SingleFilteredTag.tags}</a>
+            <a href="#" class="btn btn-primary btn-tech">tech</a>
+        </div>
+`
+cardElement.insertAdjacentHTML("beforeend", cardMarkup)
+
+})
